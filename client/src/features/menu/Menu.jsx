2a4271b57a10eb25c18193d5bc5de0db93faab09
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-21 13:30:59 -0700

    client: connected loader function

--- client/src/features/menu/Menu.jsx
+++ client/src/features/menu/Menu.jsx
@@ -1,5 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
-  return <h1>Menu</h1>;
+    // custom hook
+    //  data from loader as menuLoader
+    const menu = useLoaderData();
+    console.log(menu);
+    return <h1>Menu</h1>;
+}
+
+export async function loader() {
+    const menu = await getMenu();
+    return menu;
 }
 
 export default Menu;
