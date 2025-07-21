bc058a2558d098064b86d9caf81d17e8d2fb5c00
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-21 11:22:38 -0700

    created client folder and moved files to the client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        hello vite
+      </div>
+      <p className="read-the-docs">
+        hello world
+      </p>
+    </>
+  )
+}
+
+export default App
