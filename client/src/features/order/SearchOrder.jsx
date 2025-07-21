e4136fea728fbfabd2f3fdd4df90661453ee48f5
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-21 15:12:38 -0700

    client: added search order number function

---
+++ client/src/features/order/SearchOrder.jsx
@@ -0,0 +1,28 @@
+// import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!query) return;
+        navigate(`/order/${query}`);
+        setQuery("");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="search order number"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+        </form>
+    );
+}
+
+export default SearchOrder;
--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -1,11 +1,16 @@
 // import React from "react";
+import { Suspense } from 'react';
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
     return (
         <header>
             <Link to="/">Fast Pizza 101 Co.</Link>
             <p>we only serve delicious pizza</p>
+            <Suspense fallback={<div>Loading order...</div>}>
+                <SearchOrder />
+            </Suspense>            
         </header>
     );
 }
