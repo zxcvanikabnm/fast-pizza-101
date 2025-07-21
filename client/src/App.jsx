495ffd8841aa8317b582c37dd115ec4772618e12
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-21 11:53:04 -0700

    client: defined routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/menu",
+        element: <Menu />,
+    },
+    {
+        path: "/cart",
+        element: <Cart />,
+    },
+    {
+        path: "/order/new",
+        element: <CreateOrder />,
+    },
+    {
+        path: "/order/:orderId",
+        element: <Order />,
+    },
+]);
 
 function App() {
-//   const [count, setCount] = useState(0)
+    //   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        hello vite
-      </div>
-      <p className="read-the-docs">
-        hello world
-      </p>
-    </>
-  )
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
-export default App
+export default App;
