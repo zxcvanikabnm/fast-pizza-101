c6075758a16d165339b5d383fdf8fda929535d2e
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-23 13:26:39 -0700

    client: set up user state using redux

--- client/src/ui/Home.jsx
+++ client/src/ui/Home.jsx
@@ -1,16 +1,20 @@
 import CreateUser from "../features/user/CreateUser";
+import { useSelector } from "react-redux";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
-    <div className="my-10 text-center px-4 sm:my-16">
-      <h1 className="text-xl font-semibold text-stone-700 mb-8 md:text-3xl">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold text-stone-700 md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username === "" ? <CreateUser /> : <Button to="/menu" type="primary">Continue ordering, {username}</Button>}
     </div>
   );
 }
