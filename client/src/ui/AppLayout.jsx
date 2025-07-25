f3cafedcfaedbc6d08705ea2587cc053cecdcf21
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-25 16:15:30 -0700

    client: removed unnecessary code from AppLayout.jsx

--- client/src/ui/AppLayout.jsx
+++ client/src/ui/AppLayout.jsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import { Suspense } from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
@@ -7,18 +6,15 @@ import { Outlet, useNavigation } from "react-router-dom";
 
 function AppLayout() {
     const navigation = useNavigation();
-    // console.log(navigation)
     const isLoading = navigation.state === "loading";
 
     return (
         <div className="font-roboto-mono grid h-screen grid-rows-[auto_1fr_auto]">
             {isLoading && <Loader />}
-            {/* {true && <Loader />} */}
 
             <Header />
             <div className="overflow-scroll">
                 <main className="max-w-3xl mx-auto">
-                    {/* <h1>pizza content</h1> */}
                     <Suspense fallback={<Loader />}>
                         <Outlet />
                     </Suspense>
